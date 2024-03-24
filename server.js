a4cc7f6b3eb4ae95c5c7de235f9c941d60aab480
sajadzirak <iriafmig29@gmail.com>
2024-03-24 19:18:39 +0330

    implementing basic room functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,19 +15,24 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "Express-chat";
 
 io.on("connection", (socket) => {
-  socket.emit("message", formatMessage(botName, "Welcome to ExpressChat"));
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+    socket.join(user.room);
 
-  socket.broadcast.emit(
-    "message",
-    formatMessage(botName, "A user has joined the chat")
-  );
+    socket.emit("message", formatMessage(botName, "Welcome to ExpressChat"));
 
-  socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, "A user has left the chat"));
+    socket.broadcast.to(user.room).emit(
+      "message",
+      formatMessage(botName, `${user.username} has joined the chat`)
+    );
   });
 
   socket.on("chatMessage", (msg) => {
     io.emit("message", formatMessage("USER", msg));
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 });
 
